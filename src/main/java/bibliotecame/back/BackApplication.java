f8443698ba3ee03d4bec15664ce58c8ba809804b
Lautaro Paskevicius <lautaro.paskevicius@ing.austral.edu.ar>
2020-08-31 13:25:50 -0300

    Revert "Sprint 1 (#6)"
    This reverts commit cd46518e5cec8c1a3ad89c9cce35dcaa1f47e14f.

--- src/main/java/bibliotecame/back/BackApplication.java
+++ src/main/java/bibliotecame/back/BackApplication.java
@@ -2,16 +2,8 @@ package bibliotecame.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@ComponentScan("bibliotecame.back.Book")
-@ComponentScan("bibliotecame.back.Author")
-@ComponentScan("bibliotecame.back.Publisher")
-@ComponentScan("bibliotecame.back.Tag")
-@ComponentScan("bibliotecame.back.User")
 public class BackApplication {
 
 	public static void main(String[] args) {
