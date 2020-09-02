c9f2185f6f8c5be7ade0dfbf3dc92c6c6f2eacc7
rocioferreirorico <rocio.ferreirorico@ing.austral.edu.ar>
2020-09-02 12:16:42 -0300

    delete user controller (has ToDo)

--- src/main/java/bibliotecame/back/User/UserController.java
+++ src/main/java/bibliotecame/back/User/UserController.java
@@ -3,12 +3,13 @@ package bibliotecame.back.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
@@ -18,12 +19,12 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("{id}")
+    @GetMapping("user/{id}")
     public ResponseEntity<UserModel> getUserModel(@PathVariable Integer id){
         return new ResponseEntity<>(this.userService.findUserById(id), HttpStatus.OK);
     }
 
-    @PostMapping()
+    @PostMapping(value = "/signup")
     public ResponseEntity<UserModel> createUser(@Valid @RequestBody UserModel userModel){
 
         if(!userService.validUser(userModel))return new ResponseEntity<>(userModel, HttpStatus.BAD_REQUEST);
@@ -33,5 +34,29 @@ public class UserController {
 
         return ResponseEntity.ok(userService.saveUser(userModel));
 
+    }
+
+    @DeleteMapping("deleteUser/{id}")
+    public ResponseEntity<Integer> deleteUser(@PathVariable Integer id){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserModel loggedIn = (UserModel) authentication.getPrincipal();
+
+        UserModel user;
+        try {
+            user = this.userService.findUserById(id);
+        } catch (RuntimeException e){
+            return new ResponseEntity<>(id, HttpStatus.BAD_REQUEST);
+        }
+
+        if(loggedIn.getId() != id){
+            return new ResponseEntity<>(id, HttpStatus.UNAUTHORIZED);
+        }
+
+        //todo: check prestamos activos, cuando existan.
+
+        userService.deleteUser(user);
+
+        return ResponseEntity.ok(id);
     }
 }
