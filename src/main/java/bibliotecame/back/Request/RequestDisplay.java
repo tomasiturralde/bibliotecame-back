b14ec07dffc9971eb622a8c587b7d9ae6072fc04
khalil <khalil.stessens@ing.austral.edu.ar>
2020-10-25 15:23:31 -0300

    Added Author to display

--- src/main/java/bibliotecame/back/Request/RequestDisplay.java
+++ src/main/java/bibliotecame/back/Request/RequestDisplay.java
@@ -7,12 +7,14 @@ public class RequestDisplay {
     private String userEmail;
     private String title;
     private RequestStatus status;
+    private String author;
 
     public RequestDisplay(RequestModel requestModel) {
         this.date = requestModel.getDate();
         this.userEmail = requestModel.getUser().getEmail();
         this.title = requestModel.getTitle();
         this.status = requestModel.getStatus();
+        this.author = requestModel.getAuthor();
     }
 
     public RequestDisplay() {
@@ -48,5 +50,13 @@ public class RequestDisplay {
 
     public void setStatus(RequestStatus status) {
         this.status = status;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 }
