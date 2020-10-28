b6a28c64b8ea0cec052667492028d15b602da539
khalil <khalil.stessens@ing.austral.edu.ar>
2020-10-28 17:53:20 -0300

    Fixed Request GetAll pagination

--- src/main/java/bibliotecame/back/Request/RequestService.java
+++ src/main/java/bibliotecame/back/Request/RequestService.java
@@ -2,8 +2,16 @@ package bibliotecame.back.Request;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.Math.toIntExact;
 
 @Service
 public class RequestService {
@@ -29,8 +37,36 @@ public class RequestService {
         return requestRepository.findAllByStatus(status,PageRequest.of(page, size));
     }
 
-    public Page<RequestModel> findAll(int page, int size){
-        return requestRepository.findAll(PageRequest.of(page,size));
+    public Page<RequestDisplay> findAllPaged(int page, int size){
+        Pageable pageable = PageRequest.of(page, size);
+        List<RequestModel> result = new ArrayList<>();
+        findAll().iterator().forEachRemaining(result::add);
+//        result = result.stream().filter(searchForm::matches).collect(Collectors.toList());
+        int total = result.size();
+        int start = toIntExact(pageable.getOffset());
+        int end = Math.min((start + pageable.getPageSize()), total);
+        List<RequestDisplay> output = new ArrayList<>();
+        if (start <= end) {
+            List<RequestModel> temp = result.subList(start, end);
+            temp.stream().forEach(requestModel -> output.add(new RequestDisplay(requestModel)));
+        }
+        return new PageImpl<>(output, pageable, result.size());
+    }
+
+    public Page<RequestDisplay> findAllPagedByStatus(int page, int size, RequestStatus status){
+        Pageable pageable = PageRequest.of(page, size);
+        List<RequestModel> result = new ArrayList<>();
+        findAll().iterator().forEachRemaining(result::add);
+        result = result.stream().filter(requestModel -> requestModel.getStatus().equals(status)).collect(Collectors.toList());
+        int total = result.size();
+        int start = toIntExact(pageable.getOffset());
+        int end = Math.min((start + pageable.getPageSize()), total);
+        List<RequestDisplay> output = new ArrayList<>();
+        if (start <= end) {
+            List<RequestModel> temp = result.subList(start, end);
+            temp.stream().forEach(requestModel -> output.add(new RequestDisplay(requestModel)));
+        }
+        return new PageImpl<>(output, pageable, result.size());
     }
 
 }
