a8e81930898eb1a210791d59ed6e55b7f56c3a9c
khalil <khalil.stessens@ing.austral.edu.ar>
2020-11-15 17:41:30 -0300

    Added demo.bibliotecame user and second email account for EmailSender

--- src/main/java/bibliotecame/back/Email/EmailSender.java
+++ src/main/java/bibliotecame/back/Email/EmailSender.java
@@ -1,5 +1,7 @@
 package bibliotecame.back.Email;
 
+import com.sun.mail.smtp.SMTPSendFailedException;
+
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
@@ -10,6 +12,9 @@ public class EmailSender {
     public void notifyWithGmail(Email email) {
         String sender = "bibliotecame.notificaciones";
         String password = "bibliotecameadmin";
+
+        String sender2 = "bibliotecame.verificaciones";
+        String password2 = "bibliotecame123";
 
         Properties props = System.getProperties();
         props.put("mail.smtp.host", "smtp.gmail.com");
@@ -32,6 +37,22 @@ public class EmailSender {
             transport.connect("smtp.gmail.com", sender, password);
             transport.sendMessage(message, message.getAllRecipients());
             transport.close();
+        }
+        catch (SMTPSendFailedException fe){
+            try {
+                message.setFrom(new InternetAddress(sender2));
+                message.addRecipient(Message.RecipientType.TO, email.recipient);
+                message.setSubject(email.subject);
+                message.setText(email.body);
+                message.setContent(email.body, "text/html; charset=utf-8");
+                Transport transport = session.getTransport("smtp");
+                transport.connect("smtp.gmail.com", sender2, password2);
+                transport.sendMessage(message, message.getAllRecipients());
+                transport.close();
+            }
+            catch (MessagingException me){
+                me.printStackTrace();
+            }
         }
         catch (MessagingException me) {
             me.printStackTrace();
